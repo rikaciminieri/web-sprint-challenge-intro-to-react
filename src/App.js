130a824513e995d80ace2bf0633a8c758da8d7bc
Rika <rikaciminieri@gmail.com>
2021-05-30 11:50:51 -0700

    Rough Styling Added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people").then((response) => {
+      const characterList = response.data;
+      console.log("characterList", characterList);
+      setCharacters(characterList);
+    });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters &&
+      characters.map((character) => {
+        return <Character character={character} />
+      })}
     </div>
   );
-}
+};
 
 export default App;
-
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,61 @@
 // Write your Character component here
+import React, { useState } from "react";
+import characters from "../App";
+import styled from "styled-components";
+
+const Wrapper = styled.div `
+  background-color: #B99976;
+  max-width: 30%;
+  margin-bottom: 0 auto;
+`
+const ButtonStyling = styled.button`
+ display:inline-block;
+ padding:0.35em 1.2em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:black;
+ text-align:center;
+ transition: all 0.2s;
+
+&:hover{
+     color:#000000;
+     background-color:#FFFFFF;
+}
+`
+const Character = ({ character }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  if (!character) {
+    return null;
+  }
+
+  const handleExpand = (e) => {
+    setIsExpanded(!isExpanded);
+  };
+  console.log(character);
+
+  return (
+      <Wrapper>
+        <h3>{character.name}</h3>
+        <ButtonStyling onClick={handleExpand}>Show Character Details</ButtonStyling>
+        {isExpanded && (
+          <div>
+            <p>Birth Year: {character.birth_year} </p>
+            <p>Gender: {character.gender}</p>
+            <ul>
+              Films:
+              {character.films.map((film) => (
+                  <li>{film}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </Wrapper>
+  );
+};
+
+export default Character;
